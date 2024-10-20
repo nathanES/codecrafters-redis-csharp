753c96b0db27c4161d86a70b5897a2d5a94bf4dc
nathan <nathan.esslinger67@gmail.com>
2024-10-20 16:15:18 +0200

    handling concurrent clients

--- src/Server.cs
+++ src/Server.cs
@@ -11,12 +11,43 @@ internal class Program
         while (true)
         {
             var socket = await server.AcceptSocketAsync(); // wait for client 
-            while(socket.Connected)
+            System.Console.WriteLine("Client connected");
+            _ = Task.Run(async() =>
             {
-                var buffer = new byte[socket.ReceiveBufferSize];
-                await socket.ReceiveAsync(buffer);
-                await socket.SendAsync(FormatResponse("PONG"));
-            }
+                try
+                {
+                    while(socket.Connected)
+                    {
+                        var buffer = new byte[1_024];
+                        System.Console.WriteLine("Receiving Request");
+                        int bytesReceived = await socket.ReceiveAsync(buffer, SocketFlags.None);
+                        System.Console.WriteLine("Request received");
+                        if(bytesReceived == 0)
+                        {
+                            System.Console.WriteLine("Connection closed");
+                            break; //Connection closed
+                        }
+    
+                        System.Console.WriteLine("Sending Response");
+                        await socket.SendAsync(FormatResponse("PONG"),SocketFlags.None );
+                        System.Console.WriteLine("Response sent");
+                    }
+                }
+                catch(Exception e)
+                {
+                    System.Console.WriteLine("Exception : " + e.Message);
+                }
+                finally
+                {
+                   if (socket.Connected)
+                   {
+                    System.Console.WriteLine("ShutingDown the connection");
+                    socket.Shutdown(SocketShutdown.Both);
+                   }
+                   System.Console.WriteLine("Closing the connection");
+                   socket.Close();
+                }
+            });
         }
 
         byte[] FormatResponse(string response)
