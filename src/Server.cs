432fec6dcba4e09884528874795236e008743123
nathan <nathan.esslinger67@gmail.com>
2024-10-20 14:32:35 +0200

    adding ping method

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,23 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 6379);
-server.Start();
-server.AcceptSocket(); // wait for client
+        TcpListener server = new TcpListener(IPAddress.Any, 6379);//Port 6379 is the default port for Redis
+        server.Start();
+        var sockets = server.AcceptSocket(); // wait for client 
+        sockets.SendAsync(FormatResponse("PONG"));
+        server.Stop();
+
+        byte[] FormatResponse(string response)
+        {
+            return Encoding.UTF8.GetBytes($"+{response}\r\n");
+        }
+    }
+}
