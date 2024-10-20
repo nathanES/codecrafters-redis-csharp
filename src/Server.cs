cbf6763859b58eccd8f3a9f442619a766610961c
nathan <nathan.esslinger67@gmail.com>
2024-10-20 15:00:24 +0200

    adding multiple pings

--- src/Server.cs
+++ src/Server.cs
@@ -6,15 +6,13 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        Console.WriteLine("Logs from your program will appear here!");
-
         TcpListener server = new TcpListener(IPAddress.Any, 6379);//Port 6379 is the default port for Redis
         server.Start();
-        var sockets = server.AcceptSocket(); // wait for client 
-        sockets.SendAsync(FormatResponse("PONG"));
-        server.Stop();
-
+        while (true)
+        {
+          var sockets = server.AcceptSocket(); // wait for client 
+          sockets.SendAsync(FormatResponse("PONG"));
+        }
         byte[] FormatResponse(string response)
         {
             return Encoding.UTF8.GetBytes($"+{response}\r\n");
